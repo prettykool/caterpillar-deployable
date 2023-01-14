b4b323576c0396c56069a7824248404cd43c5f55
pk <prettykool@protonmail.com>
2023-01-14 14:55:32 -0700

    Update settings.ts

--- settings.ts
+++ settings.ts
@@ -1,5 +1,5 @@
 // General settings for the frontend go here.
 export const caterpillarSettings = {
-  "siteName": "Parasite Instance",
-  "apiURL": "http://localhost:8080",
+  "siteName": Deno.env.get("INSTANCE_NAME"),
+  "apiURL": Deno.env.get("API_URL"),
 };
