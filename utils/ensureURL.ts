c2dad174178ba746a7ce9311da0d3a486c17a138
PK <prettykool@protonmail.com>
2023-01-14 17:13:55 -0600

    uccccccccccfffffffdfdswwwww

--- utils/ensureURL.ts
+++ utils/ensureURL.ts
@@ -3,7 +3,7 @@ import { caterpillarSettings } from "../settings.ts";
 export function ensureURL(url: string, local: string) {
   url = new URL(url);
   local = new URL(local);
-  if (url.hostname === localURL.hostname) {
+  if (url.hostname === local.hostname) {
     url.hostname = new URL(caterpillarSettings.apiURL).hostname;
   }
   return url.href;
